963c26d2929051802bdb1aa177f26dd9fa6df8ca
dmr <brickdamik1996@gmail.com>
2021-05-20 18:42:11 +0300

    Homework 2 some changes

--- src/main/java/lesson2/homework2/Client1FX/Controller.java
+++ src/main/java/lesson2/homework2/Client1FX/Controller.java
@@ -2,24 +2,29 @@ package lesson2.homework2.Client1FX;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import javax.swing.*;
+import java.io.*;
 import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     @FXML
+    public VBox mainBox;
+    @FXML
     private TextArea chatArea;
+    //Bottom panel
     @FXML
     private TextField inputTextField;
     @FXML
     private Button send;
+    //Top panel
+    @FXML
+    public HBox topPanel;
     @FXML
     private Button auth;
     @FXML
@@ -38,83 +43,112 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        start();
+        openConnection();
     }
 
-//    public void updateTopPanel() {
-//        if ((myNick == null || myNick == "") || (socket == null || socket.isClosed())) {
-//            topPanel.add(btnAuth, BorderLayout.EAST);
-//            btnAuth.setText("Connect to server");
-//            btnAuth.setEnabled(true);
-//            topPanel.remove(loginInputField);
-//            topPanel.remove(passInputField);
-//        }
-//        if ((myNick == null || myNick == "") && (socket != null && !socket.isClosed())) {
-//            btnAuth.setText("Authentication");
-//            btnAuth.setEnabled(true);
-//            topPanel.add(loginInputField);
-//            topPanel.add(passInputField);
-//            topPanel.add(btnAuth);
-//        }
-//        if ((myNick != "") && (socket != null && !socket.isClosed())) {
-//            btnAuth.setEnabled(false);
-//            topPanel.add(btnAuth, BorderLayout.EAST);
-//            btnAuth.setText("Online: " + myNick);
-//            topPanel.remove(loginInputField);
-//            topPanel.remove(passInputField);
-//        }
-//    }
+    private void openConnection() {
+        try {
+            socket = new Socket(SERVER_ADDR, SERVER_PORT);
+            in = new DataInputStream(socket.getInputStream());
+            out = new DataOutputStream(socket.getOutputStream());
 
-//    public void openConnection() {
-//        try {
-//            socket = new Socket(SERVER_ADDR, SERVER_PORT);
-//            in = new DataInputStream(socket.getInputStream());
-//            out = new DataOutputStream(socket.getOutputStream());
-//
-////            updateTopPanel();
-//            Thread t = new Thread(() -> {
-//                try {
-//                    while (socket != null && !socket.isClosed()) {
-//                        String str = in.readUTF();
-//                        if (str.startsWith("/authok ")) {
-//                            myNick = str.split("\\s")[1];
-//                            break;
-//                        }
-//                        chatArea.append(str + "\n");
-//                    }
-////                    updateTopPanel();
-//                    while (socket != null && !socket.isClosed()) {
-//                        String strFromServer = in.readUTF().trim();
-//                        if (strFromServer.equalsIgnoreCase("/end")) {
-//                            closeConnection();
-//                            break;
-//                        }
-//                        chatArea.append(strFromServer + "\n");
-//                    }
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                } finally {
-//                    closeConnection();
-//                    JOptionPane.showMessageDialog(null, "Отключение от сервера");
-//                    myNick = "";
-//                }
-//            });
-//            t.start();
-//        } catch (IOException e) {
-////            updateTopPanel();
-////            JOptionPane.showMessageDialog(null, "Не удалось подключиться к серверу");
-//            System.err.println("Не удалось подключиться к серверу");
-//            e.printStackTrace();
-//        }
-//    }
+            updateTopPanel();
+            Thread t = new Thread(() -> {
+                try {
+                    while (socket != null && !socket.isClosed()) {
+                        String str = in.readUTF();
+                        if (str.startsWith("/authok ")) {
+                            myNick = str.split("\\s")[1];
+                            break;
+                        }
+                        chatArea.appendText(str + "\n");
+                    }
+                    updateTopPanel();
+                    while (socket != null && !socket.isClosed()) {
+                        String strFromServer = in.readUTF();
+                        if (strFromServer.equalsIgnoreCase("/end")) {
+                            closeConnection();
+                            break;
+                        }
+                        chatArea.appendText(strFromServer + "\n");
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+//                    StringWriter sw = new StringWriter();
+//                    e.printStackTrace(new PrintWriter(sw));
+//                    Alert alert = new Alert(Alert.AlertType.ERROR);
+//                    alert.setHeaderText("Отключение от сервера!");
+//                    alert.getDialogPane().setExpandableContent(new ScrollPane(new TextArea(sw.toString())));
+//                    alert.showAndWait();
+
+                    showAlert(e.toString());
+                } finally {
+                    closeConnection();
+
+                    System.out.println("Отключение от сервера!");
+
+
+                    showAlert("Отключение от сервера!");
+
+                    myNick = "";
+                }
+            });
+            t.start();
+        } catch (IOException e) {
+            updateTopPanel();
+            System.err.println("Не удалось подключиться к серверу");
+            e.printStackTrace();
+
+            showAlert(e.toString());
+        }
+    }
+
+    private void showAlert(String str) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setHeaderText("Не удалось подключиться к серверу!");
+        alert.getDialogPane().setExpandableContent(new ScrollPane(new TextArea(str)));
+        alert.showAndWait();
+    }
+
+    public void updateTopPanel() {
+        if ((myNick == null || myNick == "") || (socket == null || socket.isClosed())) {
+            auth.setText("Connect to server");
+            passField.setVisible(false);
+            loginField.setVisible(false);
+            passField.setMaxWidth(0);
+            loginField.setMaxWidth(0);
+            auth.setMinWidth(topPanel.getWidth());
+            auth.setDisable(false);
+        }
+        if ((myNick == null || myNick == "") && (socket != null && !socket.isClosed())) {
+            auth.setText("Authentication");
+            passField.setVisible(true);
+            loginField.setVisible(true);
+            passField.setMaxWidth(Double.MAX_VALUE);
+            loginField.setMaxWidth(Double.MAX_VALUE);
+            auth.setMinWidth(0);
+            auth.setDisable(false);
+        }
+        if ((myNick != "" && myNick != null) && (socket != null && !socket.isClosed())) {
+            auth.setText("Online: " + myNick);
+            passField.setVisible(false);
+            loginField.setVisible(false);
+            passField.setMaxWidth(0);
+            loginField.setMaxWidth(0);
+            auth.setMinWidth(topPanel.getWidth());
+            auth.setDisable(true);
+        }
+    }
 
     public void sendMessage() {
         if (!inputTextField.getText().trim().isEmpty()) {
-            chatArea.appendText(inputTextField.getText() + "\n");
             try {
                 out.writeUTF(inputTextField.getText().trim());
             } catch (IOException e) {
                 e.printStackTrace();
+                System.out.println("Ошибка отправки сообщения");
+//                JOptionPane.showMessageDialog(null, "Ошибка отправки сообщения");
             }
             inputTextField.clear();
             inputTextField.requestFocus();
@@ -142,63 +176,22 @@ public class Controller implements Initializable {
                 e.printStackTrace();
             }
         }
-    }
-
-    private void start() {
-        try {
-//            setAuthorized(false);
-            socket = new Socket(SERVER_ADDR, SERVER_PORT);
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-            Thread t = new Thread(() -> {
-                try {
-                    while (socket != null && !socket.isClosed()) {
-                        String str = in.readUTF();
-                        if (str.startsWith("/authok ")) {
-//                            setAuthorized(true);
-                            myNick = str.split("\\s")[1];
-                            break;
-                        }
-                        chatArea.appendText(str + "\n");
-                    }
-                    while (socket != null && !socket.isClosed()) {
-                            String strFromServer = in.readUTF();
-                            if (strFromServer.equalsIgnoreCase("/end")) {
-                                closeConnection();
-                                break;
-                            }
-                            chatArea.appendText(strFromServer + "\n");
-                        }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    //                        setAuthorized(false);
-                    try {
-                        socket.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    myNick = "";
-                }
-            });
-            t.start();
-        } catch (IOException e) {
-//            showAlert("Не удалось подключиться к серверу");
-            System.out.println("Не удалось подключиться к серверу");
-            e.printStackTrace();
-        }
+        updateTopPanel();
     }
 
     public void onAuthClick() {
         if (socket == null || socket.isClosed()) {
-            start();
-        }
-        try {
-            out.writeUTF("/auth " + loginField.getText() + " " + passField.getText());
-            loginField.setText("");
-            passField.setText("");
-        } catch (Exception e) {
-            e.printStackTrace();
+            openConnection();
+        } else {
+            if (socket != null && !socket.isClosed()) {
+                try {
+                    out.writeUTF("/auth " + loginField.getText() + " " + passField.getText());
+                    loginField.setText("");
+                    passField.setText("");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
