1a03b921370e2122f2d7648be55961a6ae9b954a
dmr <brickdamik1996@gmail.com>
2021-05-20 16:12:03 +0300

    Lesson 2

--- src/main/java/lesson2/Main.java
+++ src/main/java/lesson2/Main.java
@@ -1,5 +1,7 @@
+package lesson2;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java. Уровень 3");
+        System.out.println("Lesson 2");
     }
 }
