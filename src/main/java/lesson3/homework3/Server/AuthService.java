2cb14210ad3cbcfd3503117c50b38203b1cc46cb
dmr <brickdamik1996@gmail.com>
2021-05-25 14:00:34 +0300

    Homework 3

---
+++ src/main/java/lesson3/homework3/Server/AuthService.java
@@ -0,0 +1,10 @@
+package lesson3.homework3.Server;
+
+public interface AuthService {
+    void showTable();
+    void changeNick(String oldNick, String newNick);
+    boolean isNickBusy(String newNick);
+    String getNickByLoginPass(String login, String pass);
+    void start();
+    void stop();
+}
