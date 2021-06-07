a5d93a11f2ce77f8a4d227b576fd83ec0edf7298
dmr <brickdamik1996@gmail.com>
2021-06-07 15:37:51 +0300

    Homework 6 begin

---
+++ src/main/java/lesson6/homework6/ClientFX/ClientMainFX.java
@@ -0,0 +1,32 @@
+package lesson6.homework6.ClientFX;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+
+public class ClientMainFX extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
+        primaryStage.setTitle("Client Local Chat");
+        primaryStage.setScene(new Scene(root, 600, 300));
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent t) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/lesson6/homework6/Server/ServerApp.java
@@ -0,0 +1,7 @@
+package lesson6.homework6.Server;
+
+public class ServerApp {
+    public static void main(String[] args) {
+        new MyServer();
+    }
+}
